3801f9ad6d7da8ebe3b3b0897e77dbaf440a221e
Myles Wilson <spaceboymyles@yahoo.com>
2020-03-07 11:59:47 -0800

    Added new features.

--- mom_bot.py
+++ mom_bot.py
@@ -9,23 +9,14 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TOKEN = os.getenv('DISCORD_TOKEN')
-
-#client = Client()
-client = commands.Bot(command_prefix='?')
+client = commands.Bot(command_prefix='')
 
 @client.event
-#class DiscordClient(discord.client):
 async def on_ready():
-    print(f'{client.user.name} has connected to Discord!')
-async def on_message(self, message):
-    # Whenever a user other than bot says "hi"
-    if message.author != self.user:
-        if message.content == 'hi':
-            await message.channel.send('What the frick did you say to me you lil shoot')
+    print(f'{client.user.name} has connected to Discord! <AeroMilk is the milk for you>')
 
-# Chancla command that sends gif.
-# Sends random gif each time.
-@client.command(name='chancla', help='sends chancla gif')
+# Sends random chancla gif each time.
+@client.command(name = 'chancla')
 async def chancla(ctx):
     chancla_angry = [
         'https://media.giphy.com/media/TH2TwG8loO06Y/giphy.gif',
@@ -36,5 +27,59 @@ async def chancla(ctx):
 
     response = random.choice(chancla_angry)
     await ctx.send(response)
+
+
+# Sends a very, VERY special message.
+@client.command(name = 'hi')
+async def hello(ctx):
+    await ctx.send('What the frick did you say to me you lil shoot!')
+
+
+# Makes a new channel.
+@client.command(name = 'channel')
+async def make(ctx, ChannelName = 'THE H O U S E'):
+    guild = ctx.guild
+    ExistingChannel = discord.utils.get(guild.channels, name = ChannelName)
+    if not ExistingChannel:
+        print(f'{ChannelName} has been created.')
+        await guild.create_text_channel(ChannelName)
+    if ExistingChannel:
+        await ctx.send('Can you not')
+
+
+@client.command(name = 'dm')
+async def dm(ctx):
+   await ctx.author.send('<AeroMilk is the milk for you>')
+
+
+@client.command(name = 'DM')
+async def DM(ctx, user: discord.User, *, message=None):
+   #message = '<AeroMilk is the milk for you> brought to you by an anonymous user'
+   await ctx.user.send_message('<AeroMilk is the milk for you> brought to you by an anonymous user')
+
+
+@client.command(pass_context=True)
+async def clear(ctx, amount=100):
+	channel = ctx.message.channel
+	messages = []
+	async for message in client.logs_from(channel, limit=int(amount)):
+		messages.append(message)
+	await client.delete_messages(messages)
+
+
+@client.command()
+async def hungry():
+	embed = discord.Embed(
+		title = 'Title',
+		description = 'This is a description. ',
+		colour = discord.Colour.blue()
+		)
+
+	embed.set_footer(text='This is a footer.')
+	await client.say(embed=embed)
+
+
+#@client.command(name = 'advice')
+#async def say(ctx):
 
 client.run(TOKEN)
